095a2dddd63a0f1cb9a4b8b4d8d437b67025d520
asilva35 <sistev.contacto@gmail.com>
2023-12-27 09:26:46 -0500

    Refactor API_V1.js and [[...v1]].js

--- src/pages/api/v1/[[...v1]].js
+++ src/pages/api/v1/[[...v1]].js
@@ -1,106 +1,29 @@
-import ApiAccess from '@/models/ApiAccess';
-import db from '@/utils/db';
+import { formatRequest, verifyRequest, filterRecords } from '@/utils/API_V1';
 import { getRecords } from '@/utils/customersCollections';
-import { formatRequest } from '@/utils/API_V1';
 
 export default async function handler(req, res) {
+  //FORMAT THE REQUEST
   const request = formatRequest(req);
 
-  if (!request.apikey) return res.status(401).json({ error: 'Unauthorized' });
+  //VERIFY IF THE REQUEST IS AUTHORIZED
+  const verify = await verifyRequest(request);
+  if (verify !== true) return res.status(verify.status).json(verify.response);
 
-  const verification = {
-    organizationfound: false,
-    apikeyfound: false,
-    collectionfound: false,
-    objectfound: false,
-    methodfound: false,
-  };
-  await db.connect();
-  const apiaccessDB = await ApiAccess.findOne({
-    uid: request.organization,
-  });
-  await db.disconnect();
-
-  if (!apiaccessDB)
-    return res.status(401).json({ error: 'Organization Not Found' });
-
-  apiaccessDB.apiaccess.map((apikey_item) => {
-    if (apikey_item.apikey === request.apikey.replace('Bearer ', '')) {
-      verification.apikeyfound = true;
-      apikey_item.permissions.map((permission) => {
-        if (permission.collection === request.collection) {
-          verification.collectionfound = true;
-          if (permission.object.name === request.object) {
-            verification.objectfound = true;
-            if (permission.object.methods.includes(request.method)) {
-              verification.methodfound = true;
-            }
-          }
-        }
-      });
-    }
-  });
-
-  if (!verification.apikeyfound)
-    return res.status(401).json({ error: 'Unauthorized' });
-  if (!verification.collectionfound)
-    return res.status(401).json({ error: 'Collection Not Found' });
-  if (!verification.objectfound)
-    return res.status(401).json({ error: 'Object Not Found' });
-  if (!verification.methodfound)
-    return res.status(401).json({ error: 'Action Unauthorized' });
-
+  //GET THE RECORDS
   const records = await getRecords(
     request.organization,
     request.collection,
     request.object
   );
 
+  //FILTER THE RECORDS
   if (request.params.filterBy) {
-    const filterBy = request.params.filterBy.split(',');
-    const filterValue = request.params.filterValue.split(',');
-    const filterOperator = request.params.filterOperator.split(',');
-    const filter = [];
-    filterBy.map((item, index) => {
-      filter.push({
-        field: item,
-        value: filterValue[index],
-        operator: filterOperator[index],
-      });
-    });
-    const filteredRecords = records.filter((item) => {
-      let result = true;
-      filter.map((filterItem) => {
-        if (filterItem.operator === 'eq') {
-          if (item[filterItem.field] !== filterItem.value) result = false;
-        }
-        if (filterItem.operator === 'ne') {
-          if (item[filterItem.field] === filterItem.value) result = false;
-        }
-        if (filterItem.operator === 'lt') {
-          if (item[filterItem.field] >= filterItem.value) result = false;
-        }
-        if (filterItem.operator === 'gt') {
-          if (item[filterItem.field] <= filterItem.value) result = false;
-        }
-        if (filterItem.operator === 'le') {
-          if (item[filterItem.field] > filterItem.value) result = false;
-        }
-        if (filterItem.operator === 'ge') {
-          if (item[filterItem.field] < filterItem.value) result = false;
-        }
-        if (filterItem.operator === 'like') {
-          if (!item[filterItem.field].includes(filterItem.value))
-            result = false;
-        }
-      });
-      return result;
-    });
-
-    res.status(200).json(filteredRecords);
-    return;
+    const filterRecordsResponse = filterRecords(records, request);
+    return res
+      .status(filterRecordsResponse.status)
+      .json(filterRecordsResponse.response);
   }
 
+  //RETURN THE RECORDS
   res.status(200).json(records);
-  //res.status(200).json(request);
 }
--- src/utils/API_V1.js
+++ src/utils/API_V1.js
@@ -1,3 +1,6 @@
+import ApiAccess from '@/models/ApiAccess';
+import db from '@/utils/db';
+
 function formatRequest(req) {
   const { body, method, query, cookies, headers } = req;
   const { v1 } = query;
@@ -15,4 +18,98 @@ function formatRequest(req) {
   };
 }
 
-export { formatRequest };
+//FUNCTION TO VERIFY IF THE REQUEST IS AUTHORIZED
+async function verifyRequest(request) {
+  if (!request.apikey)
+    return { status: 401, response: { error: 'Unauthorized' } };
+
+  const verification = {
+    organizationfound: false,
+    apikeyfound: false,
+    collectionfound: false,
+    objectfound: false,
+    methodfound: false,
+  };
+  await db.connect();
+  const apiaccessDB = await ApiAccess.findOne({
+    uid: request.organization,
+  });
+  await db.disconnect();
+
+  if (!apiaccessDB)
+    return { status: 401, response: { error: 'Organization Not Found' } };
+
+  apiaccessDB.apiaccess.map((apikey_item) => {
+    if (apikey_item.apikey === request.apikey.replace('Bearer ', '')) {
+      verification.apikeyfound = true;
+      apikey_item.permissions.map((permission) => {
+        if (permission.collection === request.collection) {
+          verification.collectionfound = true;
+          if (permission.object.name === request.object) {
+            verification.objectfound = true;
+            if (permission.object.methods.includes(request.method)) {
+              verification.methodfound = true;
+            }
+          }
+        }
+      });
+    }
+  });
+
+  if (!verification.apikeyfound)
+    return { status: 401, response: { error: 'Unauthorized' } };
+  if (!verification.collectionfound)
+    return { status: 401, response: { error: 'Collection Not Found' } };
+  if (!verification.objectfound)
+    return { status: 401, response: { error: 'Object Not Found' } };
+  if (!verification.methodfound)
+    return { status: 401, response: { error: 'Action Unauthorized' } };
+
+  return true;
+}
+
+//FUNCTION TO FILTER RECORDS
+function filterRecords(records, request) {
+  const filterBy = request.params.filterBy.split(',');
+  const filterValue = request.params.filterValue.split(',');
+  const filterOperator = request.params.filterOperator.split(',');
+  const filter = [];
+  filterBy.map((item, index) => {
+    filter.push({
+      field: item,
+      value: filterValue[index],
+      operator: filterOperator[index],
+    });
+  });
+  const filteredRecords = records.filter((item) => {
+    let result = true;
+    filter.map((filterItem) => {
+      if (filterItem.operator === 'eq') {
+        if (item[filterItem.field] !== filterItem.value) result = false;
+      }
+      if (filterItem.operator === 'ne') {
+        if (item[filterItem.field] === filterItem.value) result = false;
+      }
+      if (filterItem.operator === 'lt') {
+        if (item[filterItem.field] >= filterItem.value) result = false;
+      }
+      if (filterItem.operator === 'gt') {
+        if (item[filterItem.field] <= filterItem.value) result = false;
+      }
+      if (filterItem.operator === 'le') {
+        if (item[filterItem.field] > filterItem.value) result = false;
+      }
+      if (filterItem.operator === 'ge') {
+        if (item[filterItem.field] < filterItem.value) result = false;
+      }
+      if (filterItem.operator === 'like') {
+        if (!item[filterItem.field].includes(filterItem.value)) result = false;
+      }
+    });
+    return result;
+  });
+
+  return { status: 200, response: filteredRecords };
+}
+
+export { formatRequest, verifyRequest, filterRecords };
